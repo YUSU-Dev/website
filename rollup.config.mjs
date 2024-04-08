fc93613d135514a11ff77f491a9860e890d10b34
Marks Polakovs <m.polakovs@yusu.org>
2024-04-08 10:36:31 +0100

    Production builds and proper Vue imports

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,19 +1,37 @@
 //@ts-check
 import { globSync } from "glob";
 import vue from "rollup-plugin-vue";
+import { rollupImportMapPlugin as importMap } from "rollup-plugin-import-map";
+import replace from "@rollup/plugin-replace";
 import { minify } from "rollup-plugin-esbuild";
 
+const vueVersion = "3.4.21";
+
 const entrypoints = globSync("*/*.component.js", {
-    ignore: ["dist/**"],
+  ignore: ["dist/**"],
 });
 
 /** @type {import("rollup").RollupOptions} */
 export default {
-    input: entrypoints,
-    output: {
-        dir: "dist",
-        format: "es"
-    },
-    plugins: [vue({}), minify()],
-    external: ["vue"],
+  input: entrypoints,
+  output: {
+    dir: "dist",
+    format: "es",
+  },
+  plugins: [
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+      __VUE_OPTIONS_API__: "false",
+      preventAssignment: true,
+    }),
+    importMap([
+      {
+        imports: {
+          vue: `https://unpkg.com/vue@${vueVersion}/dist/vue.runtime.esm-browser.prod.js`,
+        },
+      },
+    ]),
+    vue({}),
+    minify(),
+  ],
 };
