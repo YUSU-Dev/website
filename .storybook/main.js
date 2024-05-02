63e16dd9f4d03b4445176f05440a7b1b77723534
Nathan Billis <n.billis@yusu.org>
2024-05-02 14:40:12 +0100

    Add tailwind to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,7 @@ const config = {
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
     '@storybook/addon-a11y',
+    '@storybook/addon-themes'
   ],
   framework: {
     name: "@storybook/vue3-vite",
