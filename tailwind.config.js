9aff82333f6a872c16613dc1df93addc35c29d28
danrparker <95368712+danrparker@users.noreply.github.com>
2024-05-02 14:52:52 +0100

    News

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
+        colors: {
+            'beige': '#F4E9E1'
+        },
         extend: {},
     },
     plugins: [],
