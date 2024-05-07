7423da92e71a23e9eb38b185cadc614ec2eaf4ab
danrparker <95368712+danrparker@users.noreply.github.com>
2024-05-07 13:01:22 +0100

    Update news.vue

--- News/news.vue
+++ News/news.vue
@@ -2,11 +2,14 @@
 body {
   font-family: "soleil", sans-serif !important;
 }
+.select2 {
+  width: 100% !important;
+}
 </style>
 <template>
   <div class="grid grid-cols-5 container mx-auto">
 
-    <div class="col-span-1 order-2 mb-5 pl-4">
+    <div class="col-span-5 xl:col-span-1 order-1 xl:order-2 mb-5 xl:pl-4">
 
       <div class="sticky top-4 p-6 bg-[#F4E9E1]">
         <div class="">
@@ -42,16 +45,16 @@ body {
 
     </div>
 
-    <div class="col-span-4 order-1 pr-4 mx-md-0">
+    <div class="col-span-5 xl:col-span-4 order-1 xl:order-1 xl:pr-4 mx-md-0">
       <div id="masonryRow" class="masonryRow flex flex-wrap" ref="masonryRow">
         <!-- <div v-if="News.length == 0" class="container-fluid">
           <h2 class="mb-4 text-center">No Articles Found</h2>
         </div> -->
 
         <div class="mb-4 w-full sm:w-1/2 lg:w-[33%] news-card px-2 lg:px-3 pb-2 lg:pb-3" v-for="article in News">
-          <div style="position: relative">
+          <div class="transition hover:scale-105 relative">
             <a :href="'/news/article/' + article.url_title">
-              <div class="transition hover:scale-105">
+              <div class="">
                 <div class="">
                   <div v-if="article.thumbnail" class="aspect-square bg-cover bg-center"
                     :style="{ 'background-image': 'url(' + wrapURL(article.thumbnail) + ')' }">
@@ -68,38 +71,41 @@ body {
               </div>
             </a>
 
-            <div v-if="article.categories.length" class="rounded flex absolute bg-[#40454d] top-2.5 left-2.5" style="top: 10px; left: 10px; flex-direction: column; max-width: 100%; max-height: 232.33px; overflow-y: auto;">
-                        <div class="d-flex">
-                            <i class="fa-solid fa-tag p-2" style="color: #fff;"></i>
-                            <p class="m-0 pr-2" style="display: flex; align-items: center; color: #fff;">{{ article.categories.length }}</p>
-                        </div>
-                        <div class="hide pl-2 pr-3 pb-2" style="color: #fff;">
-                            <ul style="padding-inline-start: 22px; margin-bottom: 0px; padding-left: 0px; list-style: none;">
-                                <li v-for="category in article.categories" class="zoom">
-                                  <span @click="appendCategory(category.id)" style="cursor: pointer">
-                                    &#x2022; <span style="text-decoration: underline">{{ category.name }}</span>
-                                  </span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+            <div v-if="article.categories.length"
+              class="rounded flex flex-col absolute bg-[#40454d] top-2.5 ml-2 mr-3 max-w-full max-h-[232.33px] overflow-y-auto group">
+              <div class="flex">
+                <i class="fa-solid fa-tag p-2 text-white"></i>
+                <p class="m-0 pr-2 flex items-center text-white">{{ article.categories.length }}</p>
+              </div>
+              <div class="hidden group-hover:flex">
+                <div class="pl-1 pr-5 pb-4 text-white">
+                  <ul class="ps-[10px] mb-0 list-none">
+                    <li v-for="category in article.categories">
+                      <span @click="appendCategory(category.id)" style="cursor: pointer">
+                        &#x2022; <span class="underline">{{ category.name }}</span>
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+            </div>
 
           </div>
         </div>
 
       </div>
-      <div v-if="MoreResults && !loading" class="row d-flex justify-content-center m-3">
-        <button class="rounded shadow-sm zoom" type="button" @click="moreGroups()"
-          style="border: none; border: 1px solid #dfdfdf; background-color: #ededed; ">
-          <div class="d-flex p-3">
-            <h3 class="mb-0" style="color: #555;">Load More</h3>
+      <div v-if="MoreResults && !loading" class="flex justify-center">
+        <button class="rounded shadow bg-[#F4E9E1] transition hover:scale-105" type="button" @click="moreGroups()">
+          <div class="flex items-center p-3">
+            <h3 class="flex items-center h-min text-xl leading-none">Load More</h3>
             <i class="fa-sharp fa-solid fa-chevron-down pl-2"
               style="display: flex; align-items: center; font-size: 30px; color: #555;"></i>
           </div>
         </button>
       </div>
-      <div v-if="loading" class="row d-flex justify-content-center m-5 spinner">
-        <i class="fas fa-spinner fa-spin" style="font-size: 50px;"></i>
+      <div v-if="loading" class="flex justify-center m-5 spinner">
+        <i class="fas fa-spinner fa-spin text-5xl"></i>
       </div>
     </div>
   </div>
