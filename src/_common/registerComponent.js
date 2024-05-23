12b48b6510fdb83dc007e1cd3914ff5644670cd3
Nathan Billis <n.billis@yusu.org>
2024-05-23 16:16:38 +0100

    shadowRoot: flase

--- src/_common/registerComponent.js
+++ src/_common/registerComponent.js
@@ -1,6 +1,6 @@
 import { defineCustomElement } from "vue"; // gets resolved in rollup.config.mjs
 
 export function register(name, component) {
-  const el = defineCustomElement(component);
+  const el = defineCustomElement(component, { ShadowRoot: false });
   customElements.define(`yorksu-${name}`, el);
 }
