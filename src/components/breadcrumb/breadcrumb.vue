2273af0f74f8dd041820b923e39b104f3e1348ee
Nathan Billis <n.billis@yusu.org>
2024-05-23 12:34:01 +0100

    update a colours

--- src/components/breadcrumb/breadcrumb.vue
+++ src/components/breadcrumb/breadcrumb.vue
@@ -6,10 +6,10 @@
       <!-- {breadcrumbs} -->
 
       <!--- Below is an example -->
-      <a href="/">Home</a> >
-      <a href="/"> Events</a>
+      <a class="text-black no-underline hover:underline" href="/">Home</a> >
+      <a class="text-black no-underline hover:underline" href="/"> Events</a>
 
-      <!-- <a href="{crumb_link}">{crumb_text}</a>
+      <!-- <a class="text-black no-underline hover:underline" href="{crumb_link}">{crumb_text}</a>
         {if crumb_not_last}>{/if}
         {/breadcrumbs} -->
     </p>
