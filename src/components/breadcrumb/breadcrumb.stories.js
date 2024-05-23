2906e22fcd19b13128f2c9a0c5e095b24e2425c2
Nathan Billis <n.billis@yusu.org>
2024-05-23 12:23:01 +0100

    Add breadcrumbs

---
+++ src/components/breadcrumb/breadcrumb.stories.js
@@ -0,0 +1,10 @@
+import Breadcrumb from "./breadcrumb.vue";
+
+export default {
+  title: "Elements/Breadcrumb",
+  component: Breadcrumb,
+};
+
+export const Default = {
+  args: {},
+};
---
+++ src/components/breadcrumb/breadcrumb.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- Breadcrumb Navigation -->
+  <div class="font-serif text-lg">
+    <!-- {exp:su_sections:generateBreadcrumbNew title="{embed:title}"} -->
+    <p>
+      <!-- {breadcrumbs} -->
+
+      <!--- Below is an example -->
+      <a href="/">Home</a> >
+      <a href="/"> Events</a>
+
+      <!-- <a href="{crumb_link}">{crumb_text}</a>
+        {if crumb_not_last}>{/if}
+        {/breadcrumbs} -->
+    </p>
+  </div>
+  <!-- End Breadcrumb Navigation -->
+</template>
+<script>
+export default {};
+</script>
