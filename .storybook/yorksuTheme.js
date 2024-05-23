180ea949dd29ccee060b4f401e3cec9424365a10
Nathan Billis <n.billis@yusu.org>
2024-05-23 11:31:46 +0100

    theme: update logo

--- .storybook/yorksuTheme.js
+++ .storybook/yorksuTheme.js
@@ -1,9 +1,10 @@
 import { create } from "@storybook/theming";
 
 export default create({
-    base: "light",
-    brandTitle: "York Students' Union",
-    brandUrl: "https://www.yorksu.org",
-    brandImage: "https://assets-cdn.sums.su/YU/IMG/Website/new_logo_beige.png",
-    brandTarget: "_blank",
+  base: "light",
+  brandTitle: "York Students' Union",
+  brandUrl: "https://www.yorksu.org",
+  brandImage:
+    "https://assets-cdn.sums.su/YU/website/img/yorksu-logo-black-full.png",
+  brandTarget: "_blank",
 });
