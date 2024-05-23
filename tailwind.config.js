64f5409d0ed1ed9e59c93ba4fe6160b90fe7e4b3
Nathan Billis <n.billis@yusu.org>
2024-05-23 14:25:12 +0100

    Add all buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ export default {
         beige: "#F4E9E1",
         mustard: "#f2cb50",
         "light-blue": "#D3E6EF",
-        "light-pink": "fad7d9",
+        "light-pink": "#fad7d9",
+        "primary-pink": "#FFBBBE",
+        "voice-orange": "#F2682F",
+        "advice-green": "#00A463",
       },
       screens: {
         // This is just to allow 1 more breakpoint than the default, can be changed to something else as long as its equal or smaller than 576px
