505df5c0152424fe2fd4b4cc1c51088c0b2890e0
Nathan Billis <n.billis@yusu.org>
2024-05-23 14:49:58 +0100

    add breadcrumb

--- src/components/HeroHeader/heroheader.vue
+++ src/components/HeroHeader/heroheader.vue
@@ -1,15 +1,18 @@
 <template>
   <Navbar />
   <HeroBanner :title="title" :image="image" />
+  <Breadcrumb />
 </template>
 <script>
 import Navbar from "../Navbar/navbar.vue";
 import HeroBanner from "../HeroBanner/herobanner.vue";
+import Breadcrumb from "../breadcrumb/breadcrumb.vue";
 export default {
   props: ["title", "image"],
   components: {
     Navbar,
     HeroBanner,
+    Breadcrumb,
   },
   data() {
     return {};
