f1477d401925e1a7f4289fb08737661b21cb379d
Nathan Billis <n.billis@yusu.org>
2024-05-23 15:12:34 +0100

    add tailwind back

--- src/components/Navbar/navbar.vue
+++ src/components/Navbar/navbar.vue
@@ -1,3 +1,11 @@
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+body {
+  font-family: "soleil", sans-serif !important;
+}
+</style>
 <template>
   <nav class="w-full bg-beige sticky left-0 top-0">
     <div class="container mx-auto flex justify-between px-4 3xl:px-0">
