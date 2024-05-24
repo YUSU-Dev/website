a7953f58f96bc9bbbc97fd86fa174fef162f84b8
Nathan Billis <n.billis@yusu.org>
2024-05-24 11:58:39 +0100

    taste the difference

--- src/_common/registerComponent.js
+++ src/_common/registerComponent.js
@@ -3,4 +3,12 @@ import { defineCustomElement } from "vue"; // gets resolved in rollup.config.mjs
 export function register(name, component) {
   const el = defineCustomElement(component);
   customElements.define(`yorksu-${name}`, el);
+
+  const componentShadowDom = document.querySelector(component)?.shadowRoot;
+  if (componentShadowDom) {
+    const styleLink = document.createElement("link");
+    styleLink.setAttribute("rel", "stylesheet");
+    styleLink.setAttribute("href", "components.css");
+    componentShadowDom.appendChild(styleLink);
+  }
 }
