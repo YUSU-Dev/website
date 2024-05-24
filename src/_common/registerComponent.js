7369c73c72f3fac711f7f40efa49f011ab2ec761
Nathan Billis <n.billis@yusu.org>
2024-05-24 12:08:53 +0100

    log styles

--- src/_common/registerComponent.js
+++ src/_common/registerComponent.js
@@ -2,16 +2,6 @@ import { defineCustomElement } from "vue"; // gets resolved in rollup.config.mjs
 
 export function register(name, component) {
   const el = defineCustomElement(component);
+  console.log(component.styles);
   customElements.define(`yorksu-${name}`, el);
-
-  document.querySelectorAll(component).forEach((element) => {
-    if (!element.shadowRoot) return;
-    const styleLink = document.createElement("link");
-    styleLink.setAttribute("rel", "stylesheet");
-    styleLink.setAttribute(
-      "href",
-      "https://assets.yorksu.org/components/components.css",
-    );
-    element.shadowRoot.appendChild(styleLink);
-  });
 }
