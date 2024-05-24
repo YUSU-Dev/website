1ab9431a6b41b1a945a484bd9f8eb892af0a06c9
Nathan Billis <n.billis@yusu.org>
2024-05-24 11:29:40 +0100

    include tailwind config in postcss plugin

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,10 +2,8 @@ import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 
+import tailwindConfig from "./tailwind.config.js";
+
 export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-    cssnano,
-  ]
+  plugins: [tailwindcss(tailwindConfig), autoprefixer, cssnano],
 };
