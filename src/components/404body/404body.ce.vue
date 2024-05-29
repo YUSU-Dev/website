675f08973253f0e246112eee5876948b528b63ec
probablybenallen <26507833+probablybenallen@users.noreply.github.com>
2024-05-29 12:35:34 +0100

    quicklinks styled

--- src/components/404body/404body.ce.vue
+++ src/components/404body/404body.ce.vue
@@ -24,18 +24,17 @@ export default {
 
 <template>
   <main
-    class="m-auto px-4 py-14 text-center max-w-3xl min-h-96 flex flex-col gap-8"
+    class="m-auto px-4 py-14 text-center min-h-96 flex flex-col gap-12 justify-center items-center"
   >
-    <h1 class="text-6xl">Oops! Page Not Found</h1>
-    <p class="text-xl text-gray-500">
-      We're sorry, but the page you are looking for cannot be found. It seems
-      that the link you clicked may be outdated, or the page may have been
-      removed.
-    </p>
-    <!--    <h2>Search Our Site:</h2>-->
     <div>
-      <!--      <h2 class="text-base py-2">Use the search bar below to find what you're looking for:</h2>-->
-      <!--      <h2 class="text-base py-2">Search what you're looking for:</h2>-->
+      <h1 class="text-6xl mb-6">Oops! Page Not Found</h1>
+      <p class="text-2xl text-gray-600 max-w-prose">
+        We're sorry, but the page you are looking for cannot be found. It seems
+        that the link you clicked may be outdated, or the page may have been
+        removed.
+      </p>
+    </div>
+    <div class="w-full">
       <Searchbar
         :submit-search-callback="doSearch"
         placeholder="Search for something..."
@@ -43,13 +42,13 @@ export default {
       />
     </div>
     <div>
-      <h2 class="text-base py-4">
+      <h2 class="text-lg mb-6">
         Here are a few popular pages you might be interested in:
       </h2>
       <QuickLinks />
     </div>
     <div>
-      <h2 class="text-base py-4">
+      <h2 class="text-lg mb-6">
         If you're not sure where to go next, you can always return to our
         homepage:
       </h2>
