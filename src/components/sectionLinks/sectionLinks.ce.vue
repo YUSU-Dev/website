8d903bda94c5b54a015f0a55f5e4bf54639217c2
Nathan Billis <n.billis@yusu.org>
2024-05-29 12:35:21 +0100

    sectionLinks: WIP

---
+++ src/components/sectionLinks/sectionLinks.ce.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- Section Links -->
+  <!-- {exp:su_sections:GetSidebarMenu sectionKey="{embed:section_id}" } -->
+  <!-- {if has_sections} -->
+  <div class="grid grid-rows-2 gap-4">
+    <!--- Below is an example -->
+    <div class="grid md:grid-cols-4 gap-2">
+      <!-- {sections} -->
+      <div class="btn btn-student-life px-10 text-center" href="{section_url}">
+        {section_name}
+      </div>
+      <div class="btn btn-student-life px-10 text-center" href="{section_url}">
+        {section_name}
+      </div>
+      <div class="btn btn-student-life px-10 text-center" href="{section_url}">
+        {section_name}
+      </div>
+      <!-- {/sections} -->
+    </div>
+    <!-- {sections} -->
+    <!-- {if section_has_items AND item_is_active} -->
+    <div class="grid md:grid-cols-8">
+      <div class="btn btn-student-life px-10 text-center" href="{item_link}">
+        {item_text}
+      </div>
+    </div>
+    <!-- {/if} -->
+    <!-- {/sections} -->
+  </div>
+  <!-- {/if} -->
+  <!-- {/exp:su_sections:GetSidebarMenu} -->
+  <!-- End Section Links -->
+</template>
+<script>
+import "../../main.css";
+export default {};
+</script>
---
+++ src/components/sectionLinks/sectionLinks.stories.js
@@ -0,0 +1,10 @@
+import SectionLinks from "./sectionLinks.ce.vue";
+
+export default {
+  title: "Elements/SectionLinks",
+  component: SectionLinks,
+};
+
+export const Default = {
+  args: {},
+};
