5b9d6432320b5ab8ae5a16dcf554c10b5cda4f5f
Nathan Billis <n.billis@yusu.org>
2024-06-03 13:49:46 +0100

    comment out EE tags

--- src/components/sectionLinks/sectionLinks.ce.vue
+++ src/components/sectionLinks/sectionLinks.ce.vue
@@ -1,30 +1,32 @@
 <template>
   <!-- Section Links -->
-  {exp:su_sections:GetSidebarMenu sectionKey="{embed:section_id}" } {if
-  has_sections}
+  <!-- {exp:su_sections:GetSidebarMenu sectionKey="{embed:section_id}" }
+  {if has_sections} -->
   <div class="container mx-auto py-10 mx-4 justify-center">
     <div
       class="relative flex flex-col mt-6 px-2 lg:px-3 pb-4 text-center w-full max-w-4xl"
     >
       <ul class="grid gap-4 grid-cols-1 md:grid-cols-3">
-        {sections} {if section_is_active}
-
+        {sections}
+        <!-- {if section_is_active} -->
         <li class="flex justify-center break-words">
           <a class="w-full h-full btn bg-light-blue" href="{section_url}">
             {section_name}
           </a>
         </li>
-        {if:else}
+        <!-- {if:else}
         <li class="flex justify-center break-words">
           <a class="w-full h-full btn btn-student-life" href="{section_url}">
             {section_name}
           </a>
         </li>
-        {/if} {/sections}
+        {/if} -->
+        <!-- {/sections} -->
       </ul>
-      {sections} {if section_has_items} {if section_is_active}
+      <!-- {sections} -->
+      <!-- {if section_has_items AND section_is_active} -->
       <ul class="flex flex-wrap mt-6 gap-3 list-none">
-        {sections_items}
+        <!-- {sections_items} -->
         <li class="">
           <a
             class="btn btn-secondary px-10 text-center w-full"
@@ -33,12 +35,14 @@
             {item_text}
           </a>
         </li>
-        {/sections_items}
+        <!-- {/sections_items} -->
       </ul>
-      {/if} {/if} {/sections}
+      <!-- {/if} -->
+      <!-- {/sections} -->
     </div>
   </div>
-  {/if} {/exp:su_sections:GetSidebarMenu}
+  <!-- {/if}
+  {/exp:su_sections:GetSidebarMenu} -->
   <!-- End Section Links -->
 </template>
 <script>
