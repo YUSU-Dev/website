e6a732ef6053bf02e27d91fe507e5f886a662b62
Nathan Billis <n.billis@yusu.org>
2024-06-03 13:52:59 +0100

    add subbutton colours

--- src/components/sectionLinks/sectionLinks.ce.vue
+++ src/components/sectionLinks/sectionLinks.ce.vue
@@ -8,25 +8,35 @@
     >
       <ul class="grid gap-4 grid-cols-1 md:grid-cols-3">
         {sections}
-        <!-- {if section_is_active} -->
+        <!-- {if section_is_active}
         <li class="flex justify-center break-words">
           <a class="w-full h-full btn bg-light-blue" href="{section_url}">
             {section_name}
           </a>
         </li>
-        <!-- {if:else}
+        {if:else} -->
         <li class="flex justify-center break-words">
           <a class="w-full h-full btn btn-student-life" href="{section_url}">
             {section_name}
           </a>
         </li>
-        {/if} -->
+        <!-- {/if} -->
         <!-- {/sections} -->
       </ul>
       <!-- {sections} -->
       <!-- {if section_has_items AND section_is_active} -->
       <ul class="flex flex-wrap mt-6 gap-3 list-none">
         <!-- {sections_items} -->
+        <!-- {if item_is_active}
+        <li class="">
+          <a
+            class="btn bg-primary-pink py-1 font-thin px-10 text-center w-full"
+            href="{item_link}"
+          >
+            {item_text}
+          </a>
+        </li> -->
+        {if:else}
         <li class="">
           <a
             class="btn btn-secondary px-10 text-center w-full"
@@ -35,6 +45,7 @@
             {item_text}
           </a>
         </li>
+        <!-- {/if} -->
         <!-- {/sections_items} -->
       </ul>
       <!-- {/if} -->
