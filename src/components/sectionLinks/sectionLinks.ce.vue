f0a20ee1b85f116204f6bc1398510e94fda3f30d
Nathan Billis <n.billis@yusu.org>
2024-06-03 13:39:42 +0100

    add EE tags

--- src/components/sectionLinks/sectionLinks.ce.vue
+++ src/components/sectionLinks/sectionLinks.ce.vue
@@ -1,52 +1,30 @@
 <template>
   <!-- Section Links -->
-  <!-- {exp:su_sections:GetSidebarMenu sectionKey="{embed:section_id}" } -->
-  <!-- {if has_sections} -->
-  <div class="container mx-auto py-10 justify-center">
+  {exp:su_sections:GetSidebarMenu sectionKey="{embed:section_id}" } {if
+  has_sections}
+  <div class="container mx-auto py-10 mx-4 justify-center">
     <div
       class="relative flex flex-col mt-6 px-2 lg:px-3 pb-4 text-center w-full max-w-4xl"
     >
       <ul class="grid gap-4 grid-cols-1 md:grid-cols-3">
-        <!--- Below is an example -->
-        <!-- {sections} -->
+        {sections} {if section_is_active}
+
+        <li class="flex justify-center break-words">
+          <a class="w-full h-full btn bg-light-blue" href="{section_url}">
+            {section_name}
+          </a>
+        </li>
+        {if:else}
         <li class="flex justify-center break-words">
           <a class="w-full h-full btn btn-student-life" href="{section_url}">
             {section_name}
           </a>
         </li>
-        <li class="flex justify-center break-words">
-          <a class="w-full h-full btn btn-student-life" href="{section_url}">
-            {section_name}
-          </a>
-        </li>
-        <li class="flex justify-center break-words">
-          <a class="w-full h-full btn btn-student-life" href="{section_url}">
-            {section_name}
-          </a>
-        </li>
-        <li class="flex justify-center break-words">
-          <a class="w-full h-full btn btn-student-life" href="{section_url}">
-            {section_name}
-          </a>
-        </li>
-        <li class="flex justify-center break-words">
-          <a class="w-full h-full btn btn-student-life" href="{section_url}">
-            {section_name}
-          </a>
-        </li>
-        <!-- {/sections} -->
+        {/if} {/sections}
       </ul>
-      <!-- {sections} -->
-      <!-- {if section_has_items AND item_is_active} -->
-      <ul class="flex flex-wrap mt-6 gap-3">
-        <li class="">
-          <a
-            class="btn btn-secondary px-10 text-center w-full"
-            href="{item_link}"
-          >
-            {item_text} nfenjfewkmlefmklfeml;fmmafsdkmsakl;mdsal;km
-          </a>
-        </li>
+      {sections} {if section_has_items} {if section_is_active}
+      <ul class="flex flex-wrap mt-6 gap-3 list-none">
+        {sections_items}
         <li class="">
           <a
             class="btn btn-secondary px-10 text-center w-full"
@@ -55,45 +33,12 @@
             {item_text}
           </a>
         </li>
-        <li class="">
-          <a
-            class="btn btn-secondary px-10 text-center w-full"
-            href="{item_link}"
-          >
-            {item_text}
-          </a>
-        </li>
-        <li class="">
-          <a
-            class="btn btn-secondary px-10 text-center w-full"
-            href="{item_link}"
-          >
-            {item_text}
-          </a>
-        </li>
-        <li class="">
-          <a
-            class="btn btn-secondary px-10 text-center w-full"
-            href="{item_link}"
-          >
-            {item_text}
-          </a>
-        </li>
-        <li class="">
-          <a
-            class="btn btn-secondary px-10 text-center w-full"
-            href="{item_link}"
-          >
-            {item_text}
-          </a>
-        </li>
+        {/sections_items}
       </ul>
-      <!-- {/if} -->
-      <!-- {/sections} -->
+      {/if} {/if} {/sections}
     </div>
   </div>
-  <!-- {/if} -->
-  <!-- {/exp:su_sections:GetSidebarMenu} -->
+  {/if} {/exp:su_sections:GetSidebarMenu}
   <!-- End Section Links -->
 </template>
 <script>
