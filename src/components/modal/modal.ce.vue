2bbc8f5572ee950c7105c66b97432cb468dd7e50
Nathan Billis <n.billis@yusu.org>
2024-06-04 15:21:20 +0100

    re

--- src/components/modal/modal.ce.vue
+++ src/components/modal/modal.ce.vue
@@ -1,23 +1,22 @@
 <template>
-  <button
-    data-modal-target="error-modal"
-    data-modal-toggle="default-modal"
-    class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-    type="button"
-  >
-    Toggle modal
-  </button>
   <!-- main Modal-->
-  <div id="error-modal" class="modal" tabindex="-1" role="dialog">
+  <div
+    id="error-modal"
+    class="modal"
+    :class="{ hidden: modalClosed }"
+    tabindex="-1"
+    role="dialog"
+  >
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h2 class="modal-title">Basket Error</h2>
+          <h2 class="modal-title">{{ title }}</h2>
           <button
             type="button"
             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
             data-dismiss="modal"
             aria-label="Close"
+            @click="modalClosed = !modalClosed"
           >
             <svg
               class="w-3 h-3"
@@ -38,28 +37,28 @@
           </button>
         </div>
         <div class="modal-body">
-          <p id="modal-body-error">Undefined Error!</p>
-          <!-- {if logged_in} -->
-          <!-- {if:else} -->
-          <p>
-            If you think you should be able to purchase this product please sign
-            in.
-          </p>
-          <!-- {/if} -->
+          <p id="modal-body-error">{{ errorDescription }}</p>
+          <div v-if="!signedIn">
+            <p>
+              Please sign in below. If you are still having problems please get
+              in touch with YorkSU IT Support.
+            </p>
+          </div>
         </div>
         <div class="modal-footer">
-          <!-- {if logged_in} -->
-          <button type="button" class="btn btn-primary" data-dismiss="modal">
-            Close
-          </button>
-          <!-- {if:else} -->
-          <a type="button" class="btn btn-primary" href="/sign-in/sso"
-            >Sign In</a
+          <div v-if="!signedIn">
+            <a type="button" class="btn btn-primary" href="/sign-in/sso"
+              >Sign In</a
+            >
+          </div>
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-dismiss="modal"
+            @click="modalClosed = !modalClosed"
           >
-          <button type="button" class="btn btn-primary" data-dismiss="modal">
             Close
           </button>
-          <!-- {/if} -->
         </div>
       </div>
     </div>
@@ -71,6 +70,18 @@ export default {
     signedIn: {
       type: Boolean,
       default: false,
+    },
+    modalClosed: {
+      type: Boolean,
+      default: true,
+    },
+    title: {
+      type: String,
+      default: "Error",
+    },
+    errorDescription: {
+      type: String,
+      default: "Undefined Error!",
     },
   },
 };
