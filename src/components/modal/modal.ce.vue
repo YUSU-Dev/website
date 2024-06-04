0253aef2e9dff7f08eaedb03e1df83135f6f707d
Nathan Billis <n.billis@yusu.org>
2024-06-04 17:11:04 +0100

    improve modal

--- src/components/modal/modal.ce.vue
+++ src/components/modal/modal.ce.vue
@@ -1,68 +1,76 @@
 <template>
   <!-- main Modal-->
-  <div
-    id="error-modal"
-    class="modal"
-    :class="{ hidden: modalClosed }"
-    tabindex="-1"
-    role="dialog"
-  >
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h2 class="modal-title">{{ title }}</h2>
-          <button
-            type="button"
-            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-            data-dismiss="modal"
-            aria-label="Close"
-            @click="modalClosed = !modalClosed"
-          >
-            <svg
-              class="w-3 h-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 14"
+  <Teleport to="body">
+    <div
+      v-show="!modalClosed"
+      id="error-modal"
+      class="modal"
+      aria-label="Error Modal"
+      tabindex="-1"
+      role="dialog"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h2 class="modal-title">{{ title }}</h2>
+            <button
+              type="button"
+              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              data-dismiss="modal"
+              aria-label="Close"
+              @click="modalClosed = !modalClosed"
             >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-              />
-            </svg>
-            <span class="sr-only">Close modal</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <p id="modal-body-error">{{ errorDescription }}</p>
-          <div v-if="!signedIn">
-            <p>
-              Please sign in below. If you are still having problems please get
-              in touch with YorkSU IT Support.
-            </p>
+              <svg
+                class="w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                />
+              </svg>
+              <span class="sr-only">Close modal</span>
+            </button>
           </div>
-        </div>
-        <div class="modal-footer">
-          <div v-if="!signedIn">
-            <a type="button" class="btn btn-primary" href="/sign-in/sso"
-              >Sign In</a
+          <div class="modal-body">
+            <p id="modal-body-error">{{ errorDescription }}</p>
+            <div v-if="!signedIn">
+              <p>
+                Please sign in below. If you are still having problems please
+                get in touch with
+                <a
+                  href="mailto:it@yorksu.org"
+                  class="text-blue-800 hover:underline"
+                  >YorkSU IT Support</a
+                >.
+              </p>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <div v-if="!signedIn">
+              <a type="button" class="btn btn-primary" href="/sign-in/sso"
+                >Sign In</a
+              >
+            </div>
+            <button
+              type="button"
+              class="btn btn-primary"
+              data-dismiss="modal"
+              @click="modalClosed = !modalClosed"
             >
+              Close
+            </button>
           </div>
-          <button
-            type="button"
-            class="btn btn-primary"
-            data-dismiss="modal"
-            @click="modalClosed = !modalClosed"
-          >
-            Close
-          </button>
         </div>
       </div>
     </div>
-  </div>
+  </Teleport>
 </template>
 <script>
 export default {
