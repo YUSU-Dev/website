0524451c1bffdce7a4ab837ac05ad0e1edcfb211
Nathan Billis <n.billis@yusu.org>
2024-06-04 10:10:41 +0100

    news: add select2 css

--- src/components/news/news.ce.vue
+++ src/components/news/news.ce.vue
@@ -80,6 +80,11 @@
     href="https://use.fontawesome.com/releases/v6.5.1/css/all.css"
     crossorigin="anonymous"
   />
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css"
+    crossorigin="anonymous"
+  />
 </template>
 <style>
 @tailwind base;
