e6fcb795759e0b10ba527fd3456efaa5c10f7d3e
Nathan Billis <n.billis@yusu.org>
2024-06-13 12:34:03 +0100

    cc: remove inset-0

--- src/components/cookie-consent/cookie-consent.ce.vue
+++ src/components/cookie-consent/cookie-consent.ce.vue
@@ -3,7 +3,7 @@
   <Teleport to="body">
     <div
       id="error-modal"
-      class="align-end fixed bottom-0 left-0 flex justify-start overflow-y-auto overflow-x-hidden md:inset-0"
+      class="align-end fixed bottom-0 left-0 flex justify-start overflow-y-auto overflow-x-hidden"
       aria-label="Cookie Consent Modal"
       role="dialog"
       v-if="displayCookieConsent"
