5147c815201335fc083f628674ec437531d44fed
Nathan Billis <nathanbillis@users.noreply.github.com>
2024-06-14 11:58:47 +0100

    add dark mode to modals (#81)

--- src/components/cookie-consent/cookie-consent.ce.vue
+++ src/components/cookie-consent/cookie-consent.ce.vue
@@ -1,48 +1,45 @@
 <template>
-  <!-- main Modal-->
-  <Teleport to="body">
-    <div
-      id="error-modal"
-      class="align-end fixed bottom-0 left-0 flex justify-start overflow-y-auto overflow-x-hidden"
-      aria-label="Cookie Consent Modal"
-      role="dialog"
-      v-if="displayCookieConsent"
-    >
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h2 class="modal-title">We value your privacy</h2>
+  <div
+    id="error-modal"
+    class="align-end fixed bottom-0 left-0 flex justify-start overflow-y-auto overflow-x-hidden"
+    aria-label="Cookie Consent Modal"
+    role="dialog"
+    v-if="displayCookieConsent"
+  >
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h2 class="modal-title">We value your privacy</h2>
+        </div>
+        <div class="modal-body">
+          <div>
+            <p>
+              We use necessary cookies to ensure you get the best experience on
+              our website. By continuing on our site you are agreeing to using
+              cookies to help us understand usage and how to improve it. View
+              our
+              <a
+                href="/about-us/organisational-policies/cookie-policy"
+                class="text-blue-800 underline dark:text-blue-400"
+                >Cookie Policy</a
+              >.
+            </p>
           </div>
-          <div class="modal-body">
-            <div>
-              <p>
-                We use necessary cookies to ensure you get the best experience
-                on our website. By continuing on our site you are agreeing to
-                using cookies to help us understand usage and how to improve it.
-                View our
-                <a
-                  href="/about-us/organisational-policies/cookie-policy"
-                  class="text-blue-800 hover:underline"
-                  >Cookie Policy</a
-                >.
-              </p>
-            </div>
-          </div>
-          <div
-            class="flex items-center justify-items-end gap-3 rounded-b border-t border-gray-200 p-4 md:p-5 dark:border-gray-600"
+        </div>
+        <div
+          class="flex items-center justify-items-end gap-3 rounded-b border-t border-gray-200 p-4 md:p-5 dark:border-gray-600"
+        >
+          <button
+            type="button"
+            class="btn btn-primary"
+            @click="acceptCookies()"
           >
-            <button
-              type="button"
-              class="btn btn-primary"
-              @click="acceptCookies()"
-            >
-              Close
-            </button>
-          </div>
+            Close
+          </button>
         </div>
       </div>
     </div>
-  </Teleport>
+  </div>
 </template>
 <script>
 import {
