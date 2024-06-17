0adf94b8db3d37f98f13a8299185ffbe16353d00
Nathan Billis <nathanbillis@users.noreply.github.com>
2024-06-17 14:44:53 +0100

    make the sabbList a prop (#86)

--- src/components/Sabbs/sabbs.ce.vue
+++ src/components/Sabbs/sabbs.ce.vue
@@ -25,53 +25,11 @@
 <script>
 import SabbTile from "../sabb-tile/sabb-tile.ce.vue";
 export default {
-  data() {
-    return {
-      SabbsList: [
-        {
-          name: "Stare",
-          position: "Union Affairs Officer",
-          img: "https://assets-cdn.sums.su/YU/elections2024/test-headshot.jpg",
-          url: "/your-voice/president",
-        },
-        {
-          name: "Fridge Chonk",
-          position: "Union Development Officer",
-          img: "https://assets-cdn.sums.su/YU/elections2024/test-headshot-2.jpg",
-          url: "/your-voice/president",
-        },
-        {
-          name: "Spilled",
-          position: "Union Purrfection officer",
-          img: "https://assets-cdn.sums.su/YU/elections2024/test-headshot-3.jpg",
-          url: "/your-voice/president",
-        },
-        {
-          name: "Christopher Charles Nethercott",
-          position: "Meowster of Ceremony",
-          img: "https://assets-cdn.sums.su/YU/elections2024/test-headshot-4.jpg",
-          url: "/your-voice/president",
-        },
-        {
-          name: "Noice",
-          position: "Cat",
-          img: "https://assets-cdn.sums.su/YU/elections2024/test-headshot-5.jpg",
-          url: "/your-voice/president",
-        },
-        {
-          name: "One Smiley Boi",
-          position: "You’ve cat to be kitten me",
-          img: "https://assets-cdn.sums.su/YU/elections2024/test-headshot-6.jpg",
-          url: "/your-voice/president",
-        },
-        {
-          name: "Anger",
-          position: "Purr-plexed",
-          img: "https://assets-cdn.sums.su/YU/elections2024/test-headshot.jpg",
-          url: "/your-voice/president",
-        },
-      ],
-    };
+  props: {
+    SabbsList: {
+      type: Array,
+      required: true,
+    },
   },
   components: {
     SabbTile,
