d8e18ba99412d7b4ad185aaf0275aa94548c37b4
Nathan Billis <nathanbillis@users.noreply.github.com>
2024-06-17 16:24:42 +0100

    add default (#90)

--- src/components/Sabbs/sabbs.ce.vue
+++ src/components/Sabbs/sabbs.ce.vue
@@ -29,6 +29,50 @@ export default {
     SabbsList: {
       type: Array,
       required: true,
+      default: () => [
+        {
+          name: "Stare",
+          position: "Union Affairs Officer",
+          img: "https://assets-cdn.sums.su/YU/elections2024/test-headshot.jpg",
+          url: "/your-voice/president",
+        },
+        {
+          name: "Fridge Chonk",
+          position: "Union Development Officer",
+          img: "https://assets-cdn.sums.su/YU/elections2024/test-headshot-2.jpg",
+          url: "/your-voice/president",
+        },
+        {
+          name: "Spilled",
+          position: "Union Purrfection officer",
+          img: "https://assets-cdn.sums.su/YU/elections2024/test-headshot-3.jpg",
+          url: "/your-voice/president",
+        },
+        {
+          name: "Christopher Charles Nethercott",
+          position: "Meowster of Ceremony",
+          img: "https://assets-cdn.sums.su/YU/elections2024/test-headshot-4.jpg",
+          url: "/your-voice/president",
+        },
+        {
+          name: "Noice",
+          position: "Cat",
+          img: "https://assets-cdn.sums.su/YU/elections2024/test-headshot-5.jpg",
+          url: "/your-voice/president",
+        },
+        {
+          name: "One Smiley Boi",
+          position: "You’ve cat to be kitten me",
+          img: "https://assets-cdn.sums.su/YU/elections2024/test-headshot-6.jpg",
+          url: "/your-voice/president",
+        },
+        {
+          name: "Anger",
+          position: "Purr-plexed",
+          img: "https://assets-cdn.sums.su/YU/elections2024/test-headshot.jpg",
+          url: "/your-voice/president",
+        },
+      ],
     },
   },
   components: {
