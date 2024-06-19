98312d55ea6ad73807a92987dc626b2394e66c38
Nathan Billis <nathanbillis@users.noreply.github.com>
2024-06-19 12:28:35 +0100

    Add Z-index to modals (#96)
    * Add Z-index to modals
    
    * fix tab index best practices

--- src/components/modal/modal.ce.vue
+++ src/components/modal/modal.ce.vue
@@ -2,7 +2,7 @@
   <div
     v-show="!modalClosed"
     id="error-modal"
-    class="modal flex items-start"
+    class="modal z-50 flex items-start"
     aria-label="Error Modal"
     tabindex="-1"
     role="dialog"
