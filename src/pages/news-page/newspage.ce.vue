5a42fa08dc711121460d9f0c3dc50926093880bb
danrparker <95368712+danrparker@users.noreply.github.com>
2024-06-20 13:18:19 +0100

    Fixed overlapping issue, added NewsPage (#102)
    * Fixed overlapping issue, added NewsPage
    
    * fix blank header
    
    ---------
    
    Co-authored-by: Nathan Billis <n.billis@yusu.org>

---
+++ src/pages/news-page/newspage.ce.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="skiptocontent"><a href="#maincontent">skip to main content</a></div>
+  <HeroHeader
+    image="https://assets-cdn.sums.su/YU/website/img/Banners/1500x400_Web_Banners_Student_Life.jpg"
+    title="News"
+  />
+  <div
+    id="maincontent"
+    class="container mx-auto flex flex-col items-center justify-center py-20"
+  >
+    <News />
+  </div>
+  <Footer />
+</template>
+<script>
+import HeroHeader from "../../components/HeroHeader/heroheader.ce.vue";
+import Footer from "../../components/Footer/footer.ce.vue";
+import News from "../../components/news/news.ce.vue";
+export default {
+  props: ["title", "componenttitle", "selectedparents"],
+  components: {
+    HeroHeader,
+    Footer,
+    News,
+  },
+  methods: {},
+};
+</script>
---
+++ src/pages/news-page/newspage.stories.js
@@ -0,0 +1,10 @@
+import "./newspage.component.js";
+
+export default {
+  title: "Pages/NewsPage",
+  component: "newspage",
+};
+
+export const Default = {
+  args: {},
+};
