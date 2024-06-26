9290f10f6129c0ef7ea6841f6d737366db13b194
Nathan Billis <n.billis@yusu.org>
2024-06-26 10:38:18 +0100

    remove unused test

--- src/components/Events/events.stories.js
+++ src/components/Events/events.stories.js
@@ -1,6 +1,4 @@
 import "./events.component.js";
-import { userEvent } from "@storybook/test";
-import { within as shadowWithin } from "shadow-dom-testing-library";
 
 export default {
   title: "Components/Events",
@@ -9,14 +7,6 @@ export default {
 
 export const Default = {
   args: {},
-};
-
-export const EventFiltering = {
-  play: async ({ canvasElement }) => {
-    const canvas = shadowWithin(canvasElement);
-
-    await userEvent.click(canvas.getByShadowTestId("activity-select"));
-  },
 };
 
 export const ShortView = {
