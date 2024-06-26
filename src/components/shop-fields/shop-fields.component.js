97b11527f3215c36279a632eda43c007d11edcb4
Nathan Billis <n.billis@yusu.org>
2024-06-26 10:47:03 +0100

    initial commit

---
+++ src/components/shop-fields/shop-fields.component.js
@@ -0,0 +1,4 @@
+import { register } from "../../_common/registerComponent";
+import ShopFields from "./shop-fields.ce.vue";
+
+register("shop-fields", ShopFields);
---
+++ src/components/shop-fields/shop-fields.stories.js
@@ -0,0 +1,8 @@
+import "./shop-fields.component.js";
+
+export default {
+  title: "Components/Shop Fields",
+  component: "shop-fields",
+};
+
+export const Default = {};
