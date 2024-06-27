193db53469dec401b7048a98f8b51220bf210ecd
Nathan Billis <n.billis@yusu.org>
2024-06-27 12:03:08 +0100

    refactor section links to use a shared component

---
+++ src/components/section-link/section-link.ce.vue
@@ -0,0 +1,52 @@
+<template>
+  <Button
+    v-if="sectionIsActive"
+    :title="sectionName"
+    :type="type"
+    :url="sectionUrl"
+  />
+</template>
+
+<script>
+import Button from "../button/button.ce.vue";
+export default {
+  name: "SectionLink",
+  props: {
+    sectionName: {
+      type: String,
+      required: true,
+    },
+    sectionUrl: {
+      type: String,
+      required: true,
+    },
+    sectionIsActive: {
+      type: Boolean,
+      required: true,
+    },
+    isSectionItem: {
+      type: Boolean,
+      default: false,
+    },
+    sectionArea: {
+      type: String,
+      default: "primary",
+    },
+  },
+  data() {
+    return {
+      type: "",
+    };
+  },
+  components: {
+    Button,
+  },
+  created() {
+    if (this.isSectionItem) {
+      this.type = "secondary";
+    } else {
+      this.type = this.sectionArea;
+    }
+  },
+};
+</script>
--- src/components/sectionLinks/sectionLinks.ce.vue
+++ src/components/sectionLinks/sectionLinks.ce.vue
@@ -8,44 +8,28 @@
     >
       <ul class="grid grid-cols-1 gap-4 md:grid-cols-3">
         <!-- {sections} -->
-        <!-- {if section_is_active}
         <li class="flex justify-center break-words">
-          <a class="w-full h-full btn bg-light-blue" href="{section_url}">
-            {section_name}
-          </a>
+          <SectionLink
+            :section-is-active="section_is_active"
+            :section-name="item_text"
+            :section-url="item_link"
+            :section-area="advice"
+          />
         </li>
-        {if:else} -->
-        <li class="flex justify-center break-words">
-          <a class="btn btn-student-life h-full w-full" href="{section_url}">
-            {section_name}
-          </a>
-        </li>
-        <!-- {/if} -->
         <!-- {/sections} -->
       </ul>
       <!-- {sections} -->
       <!-- {if section_has_items AND section_is_active} -->
       <ul class="mt-6 flex list-none flex-wrap gap-3">
         <!-- {sections_items} -->
-        <!-- {if item_is_active}
         <li class="">
-          <a
-            class="btn bg-primary-pink py-1 font-thin px-10 text-center w-full"
-            href="{item_link}"
-          >
-            {item_text}
-          </a>
-        </li> -->
-        <!-- {if:else} -->
-        <li class="">
-          <a
-            class="btn btn-secondary w-full px-10 text-center"
-            href="{item_link}"
-          >
-            {item_text}
-          </a>
+          <SectionLink
+            :section-is-active="item_is_active"
+            :section-name="item_text"
+            :section-url="item_link"
+            :is-section-item="true"
+          />
         </li>
-        <!-- {/if} -->
         <!-- {/sections_items} -->
       </ul>
       <!-- {/if} -->
@@ -56,6 +40,30 @@
   {/exp:su_sections:GetSidebarMenu} -->
   <!-- End Section Links -->
 </template>
+<!-- eslint-disable vue/prop-name-casing -->
 <script>
-export default {};
+import SectionLink from "../section-link/section-link.ce.vue";
+export default {
+  props: {
+    section_is_active: {
+      type: Boolean,
+      required: true,
+    },
+    item_text: {
+      type: String,
+      required: true,
+    },
+    item_link: {
+      type: String,
+      required: true,
+    },
+    item_is_active: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  components: {
+    SectionLink,
+  },
+};
 </script>
