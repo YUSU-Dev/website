95d20718100a2e7bd3d55e479013c8012034b9a0
James Pursglove <92054647+JP5457@users.noreply.github.com>
2024-07-08 15:47:50 +0100

    generating image advice a-z (#132)
    * generating image advice a-z
    
    * cleaned up
    
    * cleaned up
    
    * cleaned up
    
    * Update src/components/support-tile/support-tile.ce.vue
    
    Co-authored-by: Nathan Billis <nathanbillis@users.noreply.github.com>
    Signed-off-by: James Pursglove <92054647+JP5457@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: James Pursglove <92054647+JP5457@users.noreply.github.com>
    Co-authored-by: Nathan Billis <nathanbillis@users.noreply.github.com>

---
+++ src/components/support-tile/support-tile.stories.js
@@ -0,0 +1,15 @@
+import "./support-tile.component.js";
+
+export default {
+  title: "Elements/Support Tile",
+  component: "support-tile",
+};
+
+export const Default = {
+  args: {
+    img: "https://assets-cdn.sums.su/YU/elections2024/test-headshot.jpg",
+    name: "The Thinker",
+    position: "President",
+    url: "https://yusu.org/your-voice/president",
+  },
+};
