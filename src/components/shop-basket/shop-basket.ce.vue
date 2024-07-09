5ca31aca8373eea9c06bc2550b8938b159a2856e
Nathan Billis <n.billis@yusu.org>
2024-07-09 15:20:02 +0100

    console log time

--- src/components/shop-basket/shop-basket.ce.vue
+++ src/components/shop-basket/shop-basket.ce.vue
@@ -225,6 +225,7 @@ export default {
         await axios
           .get("https://yu-development.sums.su/shop/basket-api")
           .then((response) => {
+            console.log(response.data);
             if (response.data.basket_items != "0") {
               var correctedJsonString = self.jsonFormatter(response.data);
               var jsonData = JSON.parse(correctedJsonString);
