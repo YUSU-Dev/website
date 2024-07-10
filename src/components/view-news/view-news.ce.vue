54dce0eb1c93236a71be178c239c75fc2a7b4908
Nathan Billis <n.billis@yusu.org>
2024-07-10 11:33:05 +0100

    fix footer issue

--- src/components/view-news/view-news.ce.vue
+++ src/components/view-news/view-news.ce.vue
@@ -1,40 +1,38 @@
 <template>
-  <body>
-    <main class="container mx-auto my-7">
-      <Button title="Back to News" is-primary="true" arrow="true" url="/news" />
-      <h2 class="mb-5 mt-10 text-3xl font-bold">
-        {{ Article.title ?? "Loading..." }}
-      </h2>
-      <div class="mb-4">
-        <div class="gap-4 md:flex">
-          <img
-            :src="Article.thumbnail ?? randomImage()"
-            alt=""
-            class="h-[200px]"
-          />
-          <div>
-            <p v-if="Article.snippet" class="mb-3">{{ Article.snippet }}</p>
-            <p v-if="Article.date" class="mb-5">{{ Article.date }}</p>
-          </div>
+  <main class="container mx-auto my-7">
+    <Button title="Back to News" is-primary="true" arrow="true" url="/news" />
+    <h2 class="mb-5 mt-10 text-3xl font-bold">
+      {{ Article.title ?? "Loading..." }}
+    </h2>
+    <div class="mb-4">
+      <div class="gap-4 md:flex">
+        <img
+          :src="Article.thumbnail ?? randomImage()"
+          alt=""
+          class="h-[200px]"
+        />
+        <div>
+          <p v-if="Article.snippet" class="mb-3">{{ Article.snippet }}</p>
+          <p v-if="Article.date" class="mb-5">{{ Article.date }}</p>
         </div>
       </div>
-      <hr />
-      <article id="news-article" v-html="Article.body" class="mb-5"></article>
-      <div v-if="loading" class="animate-pulse">
-        <div class="flex-1 space-y-6 px-5 py-5">
-          <div class="sr-only">Loading</div>
+    </div>
+    <hr />
+    <article id="news-article" v-html="Article.body" class="mb-5"></article>
+    <div v-if="loading" class="animate-pulse">
+      <div class="flex-1 space-y-6 px-5 py-5">
+        <div class="sr-only">Loading</div>
+        <div class="h-2 rounded bg-slate-200"></div>
+        <div class="space-y-3">
+          <div class="grid grid-cols-3 gap-4">
+            <div class="col-span-2 h-2 rounded bg-slate-200"></div>
+            <div class="col-span-1 h-2 rounded bg-slate-200"></div>
+          </div>
           <div class="h-2 rounded bg-slate-200"></div>
-          <div class="space-y-3">
-            <div class="grid grid-cols-3 gap-4">
-              <div class="col-span-2 h-2 rounded bg-slate-200"></div>
-              <div class="col-span-1 h-2 rounded bg-slate-200"></div>
-            </div>
-            <div class="h-2 rounded bg-slate-200"></div>
-          </div>
         </div>
       </div>
-    </main>
-  </body>
+    </div>
+  </main>
 </template>
 <style>
 @import "https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css";
@@ -67,7 +65,7 @@ export default {
       return randomImageUrl("primary");
     },
   },
-  created() {
+  mounted() {
     var self = this;
     self.loading = true;
     axios
