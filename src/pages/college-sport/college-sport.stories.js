82b01d39d73b8eeb97881a44624a3a2497da19a0
danrparker <95368712+danrparker@users.noreply.github.com>
2024-07-16 12:10:29 +0100

    College Sport

---
+++ src/pages/college-sport/college-sport.stories.js
@@ -0,0 +1,13 @@
+import "./college-sport.component.js";
+
+export default {
+  title: "Pages/College Sport",
+  component: "college-sport",
+};
+
+export const Default = {
+  args: {
+    title: "College Sports",
+    selectedparents: "23",
+  },
+};
