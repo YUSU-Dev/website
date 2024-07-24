cc381b7d9b373dccd2271cc5a73e12a09e8f3927
danrparker <95368712+danrparker@users.noreply.github.com>
2024-07-24 10:07:59 +0100

    Initial

---
+++ src/components/account/account.ce.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex justify-center">
+    <ul class="flex flex-col">
+      <li class="">
+        <p>Me</p>
+      </li>
+      <li>
+        <p>Interests</p>
+      </li>
+      <li>
+        <p>Activities</p>
+      </li>
+      <li>
+        <p>Purchases</p>
+      </li>
+      <li>
+        <p>Settings</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Account",
+};
+</script>
---
+++ src/components/account/account.stories.js
@@ -0,0 +1,8 @@
+import "./account.component.js";
+
+export default {
+  title: "Components/Account",
+  component: "account",
+};
+
+export const Default = {};
