2095275a5473ced05b8862776065e9965cbdde81
Nathan Billis <n.billis@yusu.org>
2024-07-25 16:09:11 +0100

    add site searchbar

---
+++ src/components/site-searchbar/site-searchbar.ce.vue
@@ -0,0 +1,28 @@
+<template>
+  <Searchbar
+    :submit-search-callback="doSearch"
+    :initial-search-value="initialSearchValue"
+  />
+</template>
+
+<script>
+import Searchbar from "../searchbar/searchbar.ce.vue";
+export default {
+  name: "SiteSearchbar",
+  components: {
+    Searchbar,
+  },
+  methods: {
+    doSearch(value) {
+      window.location.href = `/search?q=${value}`;
+    },
+  },
+  props: {
+    initialSearchValue: {
+      type: String,
+      default: null,
+      required: false,
+    },
+  },
+};
+</script>
