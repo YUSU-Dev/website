b548f4f8ffdbb10f95f7227a7479d4e0a65867ea
James Pursglove <92054647+JP5457@users.noreply.github.com>
2024-07-25 11:21:58 +0100

    Jpursglove/web 175 (#164)
    * holding page
    
    * alt text
    
    * alt text

---
+++ src/pages/holding-page/holding-page.ce.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex h-screen items-center justify-center bg-beige shadow">
+    <div class="flex w-10/12 flex-col">
+      <img
+        class="my-4 w-10/12 md:w-6/12"
+        src="https://assets-cdn.sums.su/YU/IMG/Website/new_logo_beige.png"
+        alt="University of York Students' Union"
+        loading="lazy"
+      />
+      <h1 class="my-4 text-xl">
+        Unfortunately our website is down for planned maintenance today, to
+        allow us to prepare for unveiling our new rebranded Students' Union
+        website very soon!
+      </h1>
+      <h1 class="my-4 text-xl">
+        Come back soon to meet your new York Students' Union.
+      </h1>
+      <h1 class="my-4 text-xl">
+        To get in touch with us, you can contact us at
+        <a href="mailto:helpdesk@yorksu.org" class="text-blue-800 underline"
+          >helpdesk@yorksu.org</a
+        >.
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HoldingPage",
+};
+</script>
---
+++ src/pages/holding-page/holding-page.component.js
@@ -0,0 +1,4 @@
+import { register } from "../../_common/registerComponent";
+import HoldingPage from "./holding-page.ce.vue";
+
+register("holding-page", HoldingPage);
