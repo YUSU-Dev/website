008c003b572d4ea53f543ab4cd62b31696ff3465
Nathan Billis <nathanbillis@users.noreply.github.com>
2024-07-25 16:11:42 +0100

    add site searchbar (#177)

---
+++ src/components/site-searchbar/site-searchbar.component.js
@@ -0,0 +1,4 @@
+import { register } from "../../_common/registerComponent";
+import SiteSearchbar from "./site-searchbar.ce.vue";
+
+register("site-searchbar", SiteSearchbar);
---
+++ src/components/site-searchbar/site-searchbar.stories.js
@@ -0,0 +1,14 @@
+import "./site-searchbar.component.js";
+
+export default {
+  title: "Components/Site Searchbar",
+  component: "site-searchbar",
+};
+
+export const Default = {};
+
+export const withInitialValues = {
+  args: {
+    initialSearchValue: "Testing",
+  },
+};
