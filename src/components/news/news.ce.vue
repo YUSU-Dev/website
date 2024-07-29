307d86630f33643ea517e3ccd04fb8c5b6e19ddb
Nathan Billis <n.billis@yusu.org>
2024-07-29 16:46:55 +0100

    news: fix short view

--- src/components/news/news.ce.vue
+++ src/components/news/news.ce.vue
@@ -8,7 +8,7 @@
         :title="article.title"
         :date="article.date"
         :categories="article.categories"
-        :url="'/news/article/' + article.entry_id"
+        :url="'/news/article/' + article.url_title"
         :image="article.thumbnail"
         :append-category="appendCategory"
         :news="true"
