1dc03989dde2843f478ab677bbb98afa8a44bb32
Nathan Billis <nathanbillis@users.noreply.github.com>
2024-07-30 12:25:19 +0100

    change how activity view page works (#192)

---
+++ src/components/activities-view-page/activities-view-page.component.js
@@ -0,0 +1,4 @@
+import { register } from "../../_common/registerComponent";
+import ActivitiesViewPage from "./activities-view-page.ce.vue";
+
+register("activities-view-page", ActivitiesViewPage);
---
+++ src/components/activities-view-page/activities-view-page.stories.js
@@ -0,0 +1,19 @@
+import "./activities-view-page.component.js";
+
+export default {
+  title: "Components/Activities View Page",
+  component: "activities-view-page",
+};
+
+export const Default = {};
+
+export const ury = {
+  args: {
+    activityid: 134,
+  },
+  parameters: {
+    a11y: {
+      disable: true, // Disables the a11y check for this specific story, because it's user submitted content and will always fail.
+    },
+  },
+};
