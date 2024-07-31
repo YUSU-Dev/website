b3233482a152d907033ba241f5feface72b6d31b
James Pursglove <92054647+JP5457@users.noreply.github.com>
2024-07-31 14:07:52 +0100

    activities embed component (#200)
    * activities embed component
    
    * activities embed component

---
+++ src/components/activities-embed/activities-embed.stories.js
@@ -0,0 +1,12 @@
+import "./activities-embed.component.js";
+
+export default {
+  title: "Components/Activities Embed",
+  component: "activities-embed",
+};
+
+export const Default = {
+  args: {
+    groupIds: [12, 13, 14],
+  },
+};
