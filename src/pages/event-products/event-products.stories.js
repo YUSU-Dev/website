11b05b570b724735dd5b7ca12116d1685b336efc
Nathan Billis <nathanbillis@users.noreply.github.com>
2024-08-21 11:08:10 +0100

    Change products page to use the API (#251)

--- src/pages/event-products/event-products.stories.js
+++ src/pages/event-products/event-products.stories.js
@@ -7,26 +7,6 @@ export default {
 
 export const Default = {
   args: {
-    signed_in: true,
-    event_name: "Test Name",
-    activity_name: "Test Activity",
-    event_description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam et felis sit amet varius. Proin vitae pharetra dolor, nec rutrum orci. Mauris sagittis diam sed lacinia tincidunt. Morbi et libero sed libero scelerisque pellentesque. Sed tristique mattis scelerisque. Nulla sed ex urna. Etiam condimentum nisi ullamcorper tellus accumsan, sit amet convallis enim venenatis. Proin consectetur condimentum nulla, eu varius tellus vestibulum quis. Phasellus congue felis elit, at suscipit urna lobortis in. Etiam efficitur, arcu id sollicitudin malesuada, nibh dolor fringilla sapien, quis imperdiet est tellus a arcu. Ut bibendum nisl a elit venenatis, sit amet vulputate nibh scelerisque. Quisque malesuada, metus non mollis condimentum, ante dolor tempor leo, vitae vestibulum elit lectus ac nisl. Fusce ut magna finibus, auctor nulla ut, cursus felis. Aenean non orci et arcu consectetur tincidunt ut vitae nisi. Fusce faucibus eget neque sed fermentum.",
-    u_next_on: "1719648000",
-    date_name: "Test Date Name",
-    date_venue: "Test Venue",
-    u_start_date: "1719648000",
-    u_all_day: false,
-    date_id: "4564",
-    from_price: "£1.00",
-    external_tickets: false,
-    event_url: "test-society-trip-to-big-tesco",
-    show_products: true,
-    event_products: true,
-    product_name: "Test Product",
-    product_price: "0",
-    product_inventory: 10,
-    product_description:
-      "Lorem dadawdawdawd awd Asefsgnisuhgahg iaygfuGF UYFYAWF JegfyifuhEF RKDGJOS HORT HLAJHOIRSTJHLAEJIJ uhdguaogjearogj, diur hgiuarghk agh aghgajgaerjgioadfjg aj i",
+    date_id: "4702",
   },
 };
