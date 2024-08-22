945c6e6b0643816913b3011556d7fd06e67f9e27
danrparker <95368712+danrparker@users.noreply.github.com>
2024-08-22 11:16:20 +0100

    Self define form

---
+++ src/pages/self-definitions-form/self-definitions-form.component.js
@@ -0,0 +1,4 @@
+import { register } from "../../_common/registerComponent";
+import SelfDefinitionsForm from "./self-definitions-form.ce.vue";
+
+register("self-definitions-form", SelfDefinitionsForm);
