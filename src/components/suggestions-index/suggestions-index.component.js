1c2aaae0749650a619961d2a90468dbe3befa008
Nathan Billis <n.billis@yusu.org>
2024-09-13 13:08:54 +0100

    init files

---
+++ src/components/suggestions-index/suggestions-index.component.js
@@ -0,0 +1,4 @@
+import { register } from "../../_common/registerComponent";
+import SuggestionsIndex from "./suggestions-index.ce.vue";
+
+register("suggestions-index", SuggestionsIndex);
---
+++ src/components/suggestions-index/suggestions-index.stories.js
@@ -0,0 +1,8 @@
+import "./suggestions-index.component.js";
+
+export default {
+  title: "Components/Suggestions Index",
+  component: "suggestions-index",
+};
+
+export const Default = {};
