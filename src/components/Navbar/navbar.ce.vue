e391a3fdb255cb396a4fdfdbfba689915fc80666
danrparker <95368712+danrparker@users.noreply.github.com>
2024-09-19 10:50:51 +0100

    Added to navbar and added map button to wishlist

--- src/components/Navbar/navbar.ce.vue
+++ src/components/Navbar/navbar.ce.vue
@@ -373,6 +373,10 @@ export default {
             closed: true,
             links: [
               {
+                name: "Welcome Fair",
+                link: "/welcome-fair",
+              },
+              {
                 name: "Sports A-Z",
                 link: "/sport",
               },
