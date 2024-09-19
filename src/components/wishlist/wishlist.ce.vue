e2cbdccb9612421aa4db81d359ab497753e8b32e
danrparker <95368712+danrparker@users.noreply.github.com>
2024-09-19 16:04:41 +0100

    Added signed in prop

--- src/components/wishlist/wishlist.ce.vue
+++ src/components/wishlist/wishlist.ce.vue
@@ -4,63 +4,71 @@
       <div class="h-min w-full bg-gray-100 px-3 py-2">
         <h2 class="mb-0 text-3xl font-semibold">Activity Wishlist</h2>
       </div>
-      <!-- If they do have registered interests -->
-      <div v-if="activities[0]" class="a-z-wrap">
-        <!-- for each registered interest -->
-        <div
-          v-for="activity in activities"
-          :key="activity.id"
-          class="tile mb-4 flex justify-center pb-2 lg:pb-3"
-        >
+      <div v-if="signedIn">
+        <!-- If they do have registered interests -->
+        <div v-if="activities[0]" class="a-z-wrap">
+          <!-- for each registered interest -->
           <div
-            class="relative flex h-full w-full flex-col bg-white shadow transition md:max-w-[282px]"
+            v-for="activity in activities"
+            :key="activity.id"
+            class="tile mb-4 flex justify-center pb-2 lg:pb-3"
           >
-            <div class="flex aspect-square items-center justify-center">
-              <img
-                class="aspect-square bg-cover bg-center object-cover"
-                :src="activity.image"
-                alt=""
-                loading="lazy"
-              />
-            </div>
-            <div class="flex h-full flex-col justify-between gap-y-2 p-6">
-              <h3
-                v-if="activity.name"
-                class="mb-2 line-clamp-3 font-semibold lg:text-lg"
-              >
-                {{ activity.name }}
-              </h3>
-              <div class="flex flex-col gap-y-2">
-                <Button
-                  title="Go to activity"
-                  class="flex w-full justify-center text-center"
-                  is-primary
-                  :url="'/activities/view/' + activity.url_name"
+            <div
+              class="relative flex h-full w-full flex-col bg-white shadow transition md:max-w-[282px]"
+            >
+              <div class="flex aspect-square items-center justify-center">
+                <img
+                  class="aspect-square bg-cover bg-center object-cover"
+                  :src="activity.image"
+                  alt=""
+                  loading="lazy"
                 />
-                <!-- Can we make this url go to their membership? -->
-                <Button
-                  title="Join activity"
-                  class="flex w-full justify-center text-center"
-                  is-primary
-                  :url="'/shop?activity_id=' + activity.id"
-                />
-                <InterestButton :activity-id="activity.id" />
-                <Button
-                  title="Welcome Fair Location"
-                  class="flex w-full justify-center text-center"
-                  is-primary
-                  :url="'/map?activity=' + activity.url_name"
-                />
+              </div>
+              <div class="flex h-full flex-col justify-between gap-y-2 p-6">
+                <h3
+                  v-if="activity.name"
+                  class="mb-2 line-clamp-3 font-semibold lg:text-lg"
+                >
+                  {{ activity.name }}
+                </h3>
+                <div class="flex flex-col gap-y-2">
+                  <Button
+                    title="Go to activity"
+                    class="flex w-full justify-center text-center"
+                    is-primary
+                    :url="'/activities/view/' + activity.url_name"
+                  />
+                  <!-- Can we make this url go to their membership? -->
+                  <Button
+                    title="Join activity"
+                    class="flex w-full justify-center text-center"
+                    is-primary
+                    :url="'/shop?activity_id=' + activity.id"
+                  />
+                  <InterestButton :activity-id="activity.id" />
+                  <Button
+                    title="Welcome Fair Location"
+                    class="flex w-full justify-center text-center"
+                    is-primary
+                    :url="'/map?activity=' + activity.url_name"
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <!-- if there are registered interests -->
+        <div v-else class="flex flex-col gap-y-2 px-3">
+          <h3 class="text-lg font-semibold">Nothing to show</h3>
+          <p>You have not registered your interest in any activities.</p>
+          <Button title="Find an activity" is-primary url="/societies" />
+        </div>
       </div>
-      <!-- if there are registered interests -->
-      <div v-else class="flex flex-col gap-y-2 px-3">
-        <h3 class="text-lg font-semibold">Nothing to show</h3>
-        <p>You have not registered your interest in any activities.</p>
-        <Button title="Find an activity" is-primary url="/societies" />
+      <div v-else>
+        <div class="flex flex-col gap-y-2 px-3">
+          <h3 class="text-lg font-semibold">Sign in to view your wishlist</h3>
+          <Button title="Sign in" is-primary url="/sign-in" />
+        </div>
       </div>
     </div>
   </main>
@@ -82,6 +90,10 @@ export default {
       type: Array,
       default: null,
     },
+  },
+  signedIn: {
+    type: Boolean,
+    default: false,
   },
   data() {
     return {
