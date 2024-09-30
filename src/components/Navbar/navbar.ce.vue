714561877a6a144a0796c7a2d75322e584dc1537
Nathan Billis <nathanbillis@users.noreply.github.com>
2024-09-30 11:55:48 +0100

    remove welcome fair and references to welcome (#301)

--- src/components/Navbar/navbar.ce.vue
+++ src/components/Navbar/navbar.ce.vue
@@ -373,10 +373,6 @@ export default {
             closed: true,
             links: [
               {
-                name: "Welcome Fair",
-                link: "/welcome-fair",
-              },
-              {
                 name: "Sports A-Z",
                 link: "/sport",
               },
