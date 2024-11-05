9aae8a56e4788377248c50b9ba36f45299d8bfc1
Dan <d.parker@yusu.org>
2024-11-05 11:01:25 +0000

    Venue socials component

---
+++ src/components/venue-socials/venue-socials.ce.vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-if="venue == 'courtyard'">
+    <ActivitiesContacts
+      name="The Courtyard"
+      instagram="thecourtyard_yorksu"
+      facebook="courtyardyusubars"
+    />
+  </div>
+  <div v-else-if="venue == 'glasshouse'">
+    <ActivitiesContacts
+      name="The Glasshouse"
+      instagram="theglasshouse_yorksu"
+    />
+  </div>
+  <div v-else-if="venue == 'kitchen'">
+    <ActivitiesContacts
+      name="The Kitchen at Alcuin"
+      instagram="thekitchenatalcuin"
+      facebook="thekitchenatalcuin"
+    />
+  </div>
+  <div v-else-if="venue == 'lounge'">
+    <ActivitiesContacts name="The Lounge" instagram="thelounge_yorksu" />
+  </div>
+  <div v-else-if="venue == 'vanbrugh-arms'">
+    <ActivitiesContacts
+      name="The Vanbrugh Arms"
+      instagram="thevanbrugharms_yorksu"
+    />
+  </div>
+</template>
+
+<script>
+import ActivitiesContacts from "../ActivitiesContacts/activitiescontacts.ce.vue";
+export default {
+  name: "VenueSocials",
+  components: {
+    ActivitiesContacts,
+  },
+  props: {
+    venue: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
---
+++ src/components/venue-socials/venue-socials.component.js
@@ -0,0 +1,4 @@
+import { register } from "../../_common/registerComponent";
+import VenueSocials from "./venue-socials.ce.vue";
+
+register("venue-socials", VenueSocials);
