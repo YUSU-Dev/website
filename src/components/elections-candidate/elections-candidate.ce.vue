5aeaa53c18fe91310480ad82e7d3f561d6f65e8f
Dan <d.parker@yusu.org>
2024-11-19 16:15:22 +0000

    Upgraded to use API
    Switched over to API, just waiting on some more information on the endpoint before adding in pronouns and manifesto summary.

--- src/components/elections-candidate/elections-candidate.ce.vue
+++ src/components/elections-candidate/elections-candidate.ce.vue
@@ -1,70 +1,95 @@
 <template>
-  {embed="core-components/.hero-header" title='Candidate'
-  image_url="https://assets-cdn.sums.su/YU/website/img/Banners/1500x400_Web_Banners_Student_Voice.jpg"}
   <div class="container mx-auto">
     <div class="flex flex-col gap-y-6">
-      <yorksu-button
+      <Button
         title="Back to all candidates"
         href="/elections/candidates"
         arrow
         is-student-voice
-      ></yorksu-button>
+      />
 
       <div class="flex flex-col gap-4">
-        {exp:su_elections:electionCandidates raw="true"
-        election_id="{segment_3}" parse="inward"} {if hasCandidates == 0}
-        {if:else} {candidates} {candidate} {if {candidate_id} == {segment_4}}
-
-        <h2 class="">
-          <u>
-            {if {segment_3} == 2179} UNION AFFAIRS OFFICER {if:elseif
-            {segment_3} == 2180} UNION DEVELOPMENT OFFICER {if:elseif
-            {segment_3} == 2174} ACADEMIC OFFICER {if:elseif {segment_3} ==
-            2178} SPORTS OFFICER {if:elseif {segment_3} == 2177} EQUALITY AND
-            INCLUSION OFFICER {if:elseif {segment_3} == 2176} COMMUNITY AND
-            WELLBEING OFFICER {if:elseif {segment_3} == 2175} ACTIVITIES OFFICER
-            {/if} CANDIDATE</u
-          >
-        </h2>
+        <h2 class="text-3xl">{{ electionName }} candidate</h2>
 
         <div class="flex flex-col gap-4 md:flex-row">
           <img
+            v-if="document_photo"
             class="max-w-"
-            src="{document_photo}"
-            alt="Candidate Headshot: {name}"
+            :src="document_photo"
+            :alt="'Candidate Headshot: ' + candidate.name"
           />
 
           <div class="flex flex-col justify-end">
-            <h3 class="text-2xl">{name}</h3>
-            <h4 class="text-xl">{document_pronouns}</h4>
-
-            <!-- <h4><u>BLURB</u></h4>
-          <p>{document_blurb}</p> -->
+            <h3 class="text-2xl">{{ candidate.name }}</h3>
+            <!-- <h4 class="text-xl">{document_pronouns}</h4> -->
           </div>
         </div>
       </div>
 
       <div class="flex flex-col gap-6">
-        <div>
+        <!-- <div>
           <h3 class="text-2xl">MANIFESTO SUMMARY</h3>
           {document_manifesto_summary}
-        </div>
+        </div> -->
 
         <div>
-          <h3 class="text-2xl">MANIFESTO</h3>
-          <p>{document_manifesto}</p>
+          <article class="body-style">
+            <h3 class="text-2xl">MANIFESTO</h3>
+            <p>{{ document_manifesto }}</p>
+          </article>
         </div>
       </div>
     </div>
 
-    {/if} {/candidate} {/candidates} {/if}
-    {/exp:su_elections:electionCandidates}
+    <!-- {/if} {/candidate} {/candidates} {/if}
+    {/exp:su_elections:electionCandidates} -->
   </div>
-  {embed="core-components/.footer" vue_comp="button"}
 </template>
 
 <script>
+import axios from "../../_common/axios.mjs";
+import Button from "../button/button.ce.vue";
 export default {
   name: "ElectionsCandidate",
+  components: {
+    Button,
+  },
+  props: {
+    electionId: {
+      type: String,
+      default: "",
+    },
+    candidateId: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      electionName: "",
+      candidate: {},
+      document_manifesto: "",
+      document_photo: "",
+    };
+  },
+  mounted() {
+    this.getCandidates();
+  },
+  methods: {
+    async getCandidates() {
+      var self = this;
+      await axios
+        .get("https://pluto.sums.su/api/elections/" + this.electionId)
+        .then(function (response) {
+          self.electionName = response.data.name;
+          self.candidate = response.data.candidates.find(
+            (candidate) => candidate.id == self.candidateId,
+          );
+          self.document_manifesto = self.candidate.assets.document_manifesto;
+          self.document_photo = self.candidate.assets.document_photo;
+          console.log(self.document_manifesto);
+        });
+    },
+  },
 };
 </script>
