199a4bd28df298fb3be9639ab92526d5649ba261
danrparker <95368712+danrparker@users.noreply.github.com>
2024-11-25 10:11:27 +0000

    Candidate list "/candidates" (#328)
    * Elections candidates
    
    * Removed RON from candidates
    
    * Minor tweaks
    
    * update storybook
    
    * remove from a11y test due to false positive
    
    * Add flag for a11y test
    
    * add check for election name
    
    ---------
    
    Co-authored-by: Nathan Billis <n.billis@yusu.org>

---
+++ src/components/elections-candidates/elections-candidates.stories.js
@@ -0,0 +1,12 @@
+import "./elections-candidates.component.js";
+
+export default {
+  title: "Components/Elections/Elections Candidates",
+  component: "elections-candidates",
+};
+
+export const Default = {
+  args: {
+    electionId: "2209",
+  },
+};
