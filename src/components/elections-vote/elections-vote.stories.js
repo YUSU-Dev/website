6c4729952feaef4fc78017ee1fe0b1fa0492f5c6
danrparker <95368712+danrparker@users.noreply.github.com>
2024-12-10 10:34:09 +0000

    Inital work on voting process (#352)
    * Inital work on voting process
    
    * Fixed accessibility

---
+++ src/components/elections-vote/elections-vote.stories.js
@@ -0,0 +1,12 @@
+import "./elections-vote.component.js";
+
+export default {
+  title: "Components/Elections/Elections Vote",
+  component: "elections-vote",
+};
+
+export const Default = {
+  args: {
+    electionId: "2209",
+  },
+};
