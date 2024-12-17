02af09faefe47d9169fd2feabc3b330f00912217
Nathan Billis <n.billis@yusu.org>
2024-12-17 11:08:24 +0000

    fix {u_next_on} error causing date to display wrong

--- src/components/events-id/events-id.stories.js
+++ src/components/events-id/events-id.stories.js
@@ -41,7 +41,7 @@ export const InvalidDate = {
       "https://sums-data-public.sums.su/YU/events/images/3611_jfIbV538.jpg",
     event_description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam et felis sit amet varius. Proin vitae pharetra dolor, nec rutrum orci. Mauris sagittis diam sed lacinia tincidunt. Morbi et libero sed libero scelerisque pellentesque. Sed tristique mattis scelerisque. Nulla sed ex urna. Etiam condimentum nisi ullamcorper tellus accumsan, sit amet convallis enim venenatis. Proin consectetur condimentum nulla, eu varius tellus vestibulum quis. Phasellus congue felis elit, at suscipit urna lobortis in. Etiam efficitur, arcu id sollicitudin malesuada, nibh dolor fringilla sapien, quis imperdiet est tellus a arcu. Ut bibendum nisl a elit venenatis, sit amet vulputate nibh scelerisque. Quisque malesuada, metus non mollis condimentum, ante dolor tempor leo, vitae vestibulum elit lectus ac nisl. Fusce ut magna finibus, auctor nulla ut, cursus felis. Aenean non orci et arcu consectetur tincidunt ut vitae nisi. Fusce faucibus eget neque sed fermentum.",
-    u_next_on: "u_next_on",
+    u_next_on: "{u_next_on}",
     date_name: "Test Date Name",
     date_venue: "Test Venue",
     u_start_date: "1719648000",
