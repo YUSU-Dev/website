2b0e405d4d2a200da05ef56f0e0a6515f1672e7f
danrparker <95368712+danrparker@users.noreply.github.com>
2025-01-31 10:20:42 +0000

    Notice Bar (#381)
    * Notice Bar
    
    * Add cookie
    
    ---------
    
    Co-authored-by: Nathan Billis <n.billis@yusu.org>

---
+++ src/components/notice-bar/notice-bar.stories.js
@@ -0,0 +1,13 @@
+import "./notice-bar.component.js";
+
+export default {
+  title: "Components/Notice Bar",
+  component: "notice-bar",
+};
+
+export const Default = {
+  args: {
+    expiry: "10s",
+    cookieName: "test-bar",
+  },
+};
