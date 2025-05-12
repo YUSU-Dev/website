f6f761e05cff7d3a921065056ac708f3b4f0fe57
Dan <d.parker@yusu.org>
2025-05-12 13:08:14 +0100

    Testing vueperslides

---
+++ src/components/advice-headshots/advice-headshots.ce.vue
@@ -0,0 +1,93 @@
+<template>
+  <div style="width: 1000px; height: 300px">
+    <vueper-slides>
+      <vueper-slide v-for="i in 5" :key="i" :title="i.toString()" />
+    </vueper-slides>
+  </div>
+</template>
+
+<script>
+import { VueperSlides, VueperSlide } from "vueperslides";
+import "vueperslides/dist/vueperslides.css";
+export default {
+  name: "AdviceHeadshots",
+  components: {
+    VueperSlides,
+    VueperSlide,
+  },
+  data() {
+    return {
+      headshots: [
+        {
+          name: "Rich Andrew",
+          image:
+            "https://assets-cdn.sums.su/YU/website/img/advice/rich-andrew.jpeg",
+          title: "Advice and Welfare Manager",
+        },
+        {
+          name: "Charlotte Renshaw",
+          image:
+            "https://assets-cdn.sums.su/YU/website/img/advice/charlotte-renshaw.jpeg",
+          title: "Academic and Welfare Development Coordinator",
+        },
+        {
+          name: "Evie Cox",
+          image:
+            "https://assets-cdn.sums.su/YU/website/img/advice/evie-cox.jpeg",
+          title: "Academic and Welfare Development Coordinator",
+        },
+        {
+          name: "Matt Johnstone",
+          image:
+            "https://assets-cdn.sums.su/YU/website/img/advice/matt-johnstone.jpeg",
+          title: "Academic and Welfare Development Coordinator",
+        },
+        {
+          name: "Laura Robertson",
+          image:
+            "https://assets-cdn.sums.su/YU/website/img/advice/laura-robertson.jpeg",
+          title: "Academic and Welfare Development Coordinator",
+        },
+      ],
+      // currentIndex: 0,
+      // firstItem: null,
+      // lastItem: null,
+      // disableTransition: false,
+    };
+  },
+  // computed: {
+  //   middleIndex() {
+  //     return Math.floor(this.headshots.length / 2);
+  //   },
+  // },
+  // methods: {
+  //   shuffle(array) {
+  //     for (let i = array.length - 1; i > 0; i--) {
+  //       const j = Math.floor(Math.random() * (i + 1));
+  //       [array[i], array[j]] = [array[j], array[i]];
+  //     }
+  //     return array;
+  //   },
+  //   next() {
+  //     this.disableTransition = true;
+  //     this.$nextTick(() => {
+  //       const firstItem = this.headshots.shift();
+  //       this.headshots.push(firstItem);
+  //       this.$nextTick(() => {
+  //         this.disableTransition = false;
+  //       });
+  //     });
+  //   },
+  //   prev() {
+  //     this.disableTransition = true;
+  //     this.$nextTick(() => {
+  //       const lastItem = this.headshots.pop();
+  //       this.headshots.unshift(lastItem);
+  //       this.$nextTick(() => {
+  //         this.disableTransition = false;
+  //       });
+  //     });
+  //   },
+  // },
+};
+</script>
