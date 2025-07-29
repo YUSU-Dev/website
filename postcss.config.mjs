1e998742edff7da829d58e8ecd2d2953bbb2d951
Nathan Billis <n.billis@yorksu.org>
2025-07-29 15:45:17 +0100

    Add missing properties
    https://github.com/tailwindlabs/tailwindcss/issues/15005

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,6 @@
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 import tailwindcss from "@tailwindcss/postcss";
-
 export default {
   plugins: [tailwindcss, autoprefixer, cssnano],
 };
