e08b8c386084c3c7f16b883027f6be15a32ed905
Dan <d.parker@yusu.org>
2025-08-07 11:59:49 +0100

    Advice and Support updates

---
+++ src/pages/advice-support-a-z-page/advice-support-a-z-page.component.js
@@ -0,0 +1,4 @@
+import { register } from "../../_common/registerComponent";
+import AdviceSupportAZPage from "./advice-support-a-z-page.ce.vue";
+
+register("advice-support-a-z-page", AdviceSupportAZPage);
