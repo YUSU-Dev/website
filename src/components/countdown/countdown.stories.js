a13fb20c84742de4659b28e844ead21e1d7154fc
Dan <d.parker@yusu.org>
2025-08-13 11:17:12 +0100

    Created countdown component, added to homepage

---
+++ src/components/countdown/countdown.stories.js
@@ -0,0 +1,12 @@
+import "./countdown.component.js";
+
+export default {
+  title: "Components/Countdown",
+  component: "countdown",
+};
+
+export const Default = {
+  args: {
+    date: "2025-10-01T20:00:00",
+  },
+};
