45ff31d499c44f65121d68c33b0b6d117ccd1bdb
Dan <d.parker@yusu.org>
2025-09-11 16:14:09 +0100

    Night safe page changes

---
+++ src/pages/night-safe-page/night-safe-page.ce.vue
@@ -0,0 +1,336 @@
+<template>
+  <div id="skiptocontent"><a href="#main">skip to main content</a></div>
+  <HeroHeader
+    image="https://assets-cdn.sums.su/YU/website/img/Banners/1500x400_Web_Banners_Advice_Support.jpg"
+    title="Nightlife in York"
+  />
+  <div id="main" class="container mx-auto">
+    <!-- Main content -->
+    <div class="{page_class} body-style flex flex-col gap-y-4">
+      <!-- Main Content -->
+      <!-- Page 2263 content --->
+      <div class="flex flex-col gap-y-4">
+        <h4 dir="ltr">
+          <b id="docs-internal-guid-0ed047f1-7fff-4338-dbfe-cc65586f537f"
+            ><a
+              href="https://docs.google.com/forms/d/e/1FAIpQLSf9iSAF0q4s3VS3fgEgKtp99u7gH_kHsBJZ7rSVbGS80T73KQ/viewform?usp=sf_link"
+              >Click here to report an incident from a York SU club night, event
+              or venue</a
+            >. For immediate help on campus, call Campus Safety on +44 (0)1904
+            32 3333 if it&#39;s urgent but not dangerous, or 999.</b
+          >
+        </h4>
+
+        <p dir="ltr">
+          York is a vibrant city at night, from cosy pubs to lively clubs. As
+          your Students&rsquo; Union, we want you to enjoy it safely and
+          confidently.
+        </p>
+
+        <p dir="ltr">
+          We host weekly student-only club nights with York Parties x Voodoo
+          Events:
+        </p>
+
+        <ul>
+          <li aria-level="1" dir="ltr">
+            <p dir="ltr" role="presentation">Wednesday and Friday: Salvation</p>
+          </li>
+          <li aria-level="1" dir="ltr">
+            <p dir="ltr" role="presentation">Thursday: Flares</p>
+          </li>
+          <li aria-level="1" dir="ltr">
+            <p dir="ltr" role="presentation">Sunday: Revolution (Revs)</p>
+          </li>
+        </ul>
+
+        <p dir="ltr">
+          We also run five on-campus venues (four serving alcohol), staffed by
+          trained teams and duty managers, with anti-spiking tools available on
+          request.
+        </p>
+
+        <hr />
+        <h2 dir="ltr">
+          <b id="docs-internal-guid-0ed047f1-7fff-4338-dbfe-cc65586f537f"
+            >Going Out in the City</b
+          >
+        </h2>
+
+        <p dir="ltr">
+          Our student club nights run during term time and are student-only
+          events.&nbsp;
+        </p>
+
+        <p dir="ltr">
+          We work with our partners to make sure venues are as safe as possible
+          for students. However, if something happens on a night out we want to
+          know about it.
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSf9iSAF0q4s3VS3fgEgKtp99u7gH_kHsBJZ7rSVbGS80T73KQ/viewform?usp=sf_link"
+            >Use our reporting portal here.</a
+          >
+        </p>
+      </div>
+      <!-- End of page 2263 content -->
+      <!-- Page 2264 content --->
+      <div class="flex flex-col gap-y-4">
+        <h3 dir="ltr">
+          <b id="docs-internal-guid-0ed047f1-7fff-4338-dbfe-cc65586f537f"
+            >NightSafe</b
+          >
+        </h3>
+
+        <p dir="ltr">
+          On SU club nights, you&rsquo;ll also see
+          <a href="https://yorksu.org/activities/view/NIghtsafe"
+            >NightSafe volunteers</a
+          >
+          patrolling the city centre. Look out for the orange hi-vis jackets.
+        </p>
+
+        <p dir="ltr">
+          They are trained to support students, stay in contact with emergency
+          services and venue staff, and are there to look out for your
+          wellbeing. They&rsquo;re there to help with anything from feeling
+          unwell to getting home safely.
+        </p>
+      </div>
+      <!-- End of page 2264 content -->
+      <div class="xxs:flex-row flex flex-col gap-4">
+        <div class="">
+          <img
+            src="https://assets-cdn.sums.su/YU/website/img/advice/Night-Safe/nightsafe_group.jpg"
+            alt="Night Safe volunteers"
+            class="max-h-[300px]"
+          />
+        </div>
+        <div class="">
+          <img
+            src="https://assets-cdn.sums.su/YU/website/img/advice/Night-Safe/nightsafe.jpg"
+            alt="Night Safe volunteers"
+            class="max-h-[300px]"
+          />
+        </div>
+      </div>
+      <!-- Page 2265 content -->
+      <div class="flex flex-col gap-y-4">
+        <h3 dir="ltr">
+          <b id="docs-internal-guid-0ed047f1-7fff-4338-dbfe-cc65586f537f"
+            >River Safety</b
+          >
+        </h3>
+
+        <p dir="ltr">
+          York&rsquo;s rivers are dangerous. Never be tempted to jump in. If you
+          do accidentally end up in the water:
+        </p>
+
+        <ul>
+          <li aria-level="1" dir="ltr">
+            <p dir="ltr" role="presentation">
+              Tilt your head back, with ears submerged
+            </p>
+          </li>
+          <li aria-level="1" dir="ltr">
+            <p dir="ltr" role="presentation">
+              Relax and try to breathe normally
+            </p>
+          </li>
+          <li aria-level="1" dir="ltr">
+            <p dir="ltr" role="presentation">
+              Use your hands to help you stay afloat
+            </p>
+          </li>
+          <li aria-level="1" dir="ltr">
+            <p dir="ltr" role="presentation">
+              Don&rsquo;t worry if your legs sink&mdash;everyone floats
+              differently
+            </p>
+          </li>
+          <li aria-level="1" dir="ltr">
+            <p dir="ltr" role="presentation">
+              Spread your arms and legs for stability
+            </p>
+          </li>
+        </ul>
+
+        <p dir="ltr">These steps can help you stay safe until help arrives.</p>
+
+        <p dir="ltr">&nbsp;</p>
+
+        <h3 dir="ltr">
+          <b id="docs-internal-guid-0ed047f1-7fff-4338-dbfe-cc65586f537f"
+            >Getting Home Safely</b
+          >
+        </h3>
+
+        <ul>
+          <li aria-level="1" dir="ltr">
+            <p dir="ltr" role="presentation">
+              <a href="https://www.firstbus.co.uk/tech-bus/first-bus-app"
+                >First Bus App</a
+              >: Plan your journey, track your bus in real time, and use Space
+              Checker to see how busy it will be before you travel.
+            </p>
+          </li>
+          <li aria-level="1" dir="ltr">
+            <p dir="ltr" role="presentation">
+              Taxis: Save the
+              <a href="https://www.streamlinetaxisyork.co.uk/"
+                >Streamline Taxi</a
+              >
+              number in your phone: 01904 656565.
+            </p>
+          </li>
+          <li aria-level="1" dir="ltr">
+            <p dir="ltr" role="presentation">
+              Plan ahead: Know your route home before you head out and avoid
+              walking alone late at night.
+            </p>
+          </li>
+        </ul>
+
+        <p dir="ltr">&nbsp;</p>
+
+        <hr />
+        <h2 dir="ltr">
+          <b id="docs-internal-guid-0ed047f1-7fff-4338-dbfe-cc65586f537f"
+            >Going Out on Campus</b
+          >
+        </h2>
+
+        <p dir="ltr">
+          We run five venues on campus, four of which serve alcohol. All are
+          staffed by trained teams, with duty managers on shift to provide
+          support if needed.
+        </p>
+
+        <p dir="ltr">
+          We also stock anti-spiking tools such as Drink Covers. Please speak to
+          the staff if you&rsquo;d like to use them.
+        </p>
+
+        <p dir="ltr">&nbsp;</p>
+
+        <hr />
+        <h2 dir="ltr">
+          <b id="docs-internal-guid-0ed047f1-7fff-4338-dbfe-cc65586f537f"
+            >Reporting an Incident</b
+          >
+        </h2>
+
+        <h3 dir="ltr">
+          <b id="docs-internal-guid-0ed047f1-7fff-4338-dbfe-cc65586f537f"
+            >At a York SU club night, event, or venue</b
+          >
+        </h3>
+
+        <p dir="ltr">
+          If something concerning happens at one of our events, please know
+          you&rsquo;re not alone. You can
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSf9iSAF0q4s3VS3fgEgKtp99u7gH_kHsBJZ7rSVbGS80T73KQ/viewform?usp=sf_link"
+            >report what happened using our online reporting form.</a
+          >&nbsp;
+        </p>
+
+        <p dir="ltr">You control what happens with your report.</p>
+
+        <ul dir="ltr">
+          <li role="presentation">
+            You can let us know if you want us to reach out or not, and you can
+            also choose to do nothing. We just want to know, and support you if
+            you&rsquo;d like.
+          </li>
+          <li role="presentation">
+            You can choose if we share your details with York Parties x Voodoo
+            Events (our city club night partners). Any information you choose to
+            give will be handled securely in line with our privacy policy.<br />
+            &nbsp;
+          </li>
+        </ul>
+
+        <p dir="ltr">
+          If you have any queries or concerns about events, both on and off
+          campus, please contact events@yorksu.org.
+        </p>
+
+        <h3 dir="ltr">
+          <b id="docs-internal-guid-0ed047f1-7fff-4338-dbfe-cc65586f537f"
+            >Involving another student or a University staff member</b
+          >
+        </h3>
+
+        <p dir="ltr">
+          If your concern relates to another University of York student or staff
+          member, please use the
+          <a href="http://reportandsupport.york.ac.uk/"
+            >University&rsquo;s Report and Support tool</a
+          >.&nbsp;
+        </p>
+
+        <p dir="ltr">&nbsp;</p>
+
+        <hr />
+        <h2 dir="ltr">
+          <b id="docs-internal-guid-0ed047f1-7fff-4338-dbfe-cc65586f537f"
+            >Immediate Help</b
+          >
+        </h2>
+
+        <ul>
+          <li aria-level="1" dir="ltr">
+            <p dir="ltr" role="presentation">
+              On campus:
+              <a href="https://www.york.ac.uk/about/campus/campus-safety/"
+                >The Campus Safety Team</a
+              >
+              are available 24/7 for any safety or security issues. All officers
+              are trained in first aid and mental health first aid.
+            </p>
+
+            <ul>
+              <li aria-level="2" dir="ltr">
+                <p dir="ltr" role="presentation">Phone: 01904 32 3333</p>
+              </li>
+              <li aria-level="2" dir="ltr">
+                <p dir="ltr" role="presentation">
+                  Or download and&nbsp;<a href="https://safezoneapp.com/"
+                    >use the SafeZone app</a
+                  >, which allows you to request help and share your location
+                  with responders. (It only tracks you if you request
+                  assistance.)
+                </p>
+              </li>
+            </ul>
+          </li>
+          <li aria-level="1" dir="ltr">
+            <p dir="ltr" role="presentation">
+              In the city centre: Call 999 for police, ambulance, or fire
+              services in an emergency.
+            </p>
+          </li>
+          <li>
+            At SU club nights: Speak to venue staff, York SU Liaison,&nbsp;or
+            approach a NightSafe volunteer for immediate support.
+          </li>
+        </ul>
+      </div>
+      <!-- End of page 2265 content -->
+      <!-- End Main Content-->
+    </div>
+  </div>
+  <Footer />
+</template>
+<script>
+import HeroHeader from "../../components/HeroHeader/heroheader.ce.vue";
+import Footer from "../../components/Footer/footer.ce.vue";
+export default {
+  name: "NightSafePage",
+  components: {
+    HeroHeader,
+    Footer,
+  },
+};
+</script>
---
+++ src/pages/night-safe-page/night-safe-page.stories.js
@@ -0,0 +1,8 @@
+import "./night-safe-page.component.js";
+
+export default {
+  title: "Pages/Night Safe Page",
+  component: "night-safe-page",
+};
+
+export const Default = {};
