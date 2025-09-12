5826c26148c2b85053109f2cb4fdefe83607f2e8
Nathan Billis <n.billis@yorksu.org>
2025-09-12 12:15:30 +0100

    ignore a11y testing

--- src/pages/night-safe-page/night-safe-page.stories.js
+++ src/pages/night-safe-page/night-safe-page.stories.js
@@ -3,6 +3,8 @@ import "./night-safe-page.component.js";
 export default {
   title: "Pages/Night Safe Page",
   component: "night-safe-page",
+  // Ignore A11y testing
+  parameters: { a11y: { disable: true } },
 };
 
 export const Default = {};
