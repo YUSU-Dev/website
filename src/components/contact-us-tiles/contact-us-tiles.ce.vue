6bc4f12e66323caaeecb611eb6b6bb80cb2e33de
Dan <d.parker@yusu.org>
2025-09-30 09:45:03 +0100

    Fixed FontAwesome import

--- src/components/contact-us-tiles/contact-us-tiles.ce.vue
+++ src/components/contact-us-tiles/contact-us-tiles.ce.vue
@@ -66,8 +66,20 @@
 </template>
 
 <script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faFacebook,
+  faInstagram,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(faFacebook, faInstagram, faXTwitter);
 export default {
   name: "ContactUsTiles",
+  components: {
+    FontAwesomeIcon,
+  },
   data() {
     return {
       departments: [
