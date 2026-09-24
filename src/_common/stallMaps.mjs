54b641b75a29e1df9e66677f33e6748ccbb5615f
Lee Readman <l.readman@yorksu.org>
2026-09-24 09:57:27 +0100

    Update Welcome stall map links for 2026

--- src/_common/stallMaps.mjs
+++ src/_common/stallMaps.mjs
@@ -2,30 +2,30 @@
 export const stallMaps = [
   {
     id: "1",
-    map: "https://assets-cdn.sums.digital/YU/website/img/Welcome-Fair/sports-centre-arena.pdf",
+    map: "https://assets-cdn.sums.digital/YU/website/img/Welcome-Fair/2026/Sports-Centre-Arena.jpg",
   },
   {
     id: "2",
-    map: "https://assets-cdn.sums.digital/YU/website/img/Welcome-Fair/sports-centre-main-hall.pdf",
+    map: "https://assets-cdn.sums.digital/YU/website/img/Welcome-Fair/2026/Sports-Centre-Main-Hall.jpg",
   },
   {
     id: "3",
-    map: "https://assets-cdn.sums.digital/YU/website/img/Welcome-Fair/physics.pdf",
+    map: "https://assets-cdn.sums.digital/YU/website/img/Welcome-Fair/2026/Physics.jpg",
   },
   {
     id: "4",
-    map: "https://assets-cdn.sums.digital/YU/website/img/Welcome-Fair/central-hall.pdf",
+    map: "https://assets-cdn.sums.digital/YU/website/img/Welcome-Fair/2026/Central-Hall.jpg",
   },
   {
     id: "5",
-    map: "https://assets-cdn.sums.digital/YU/website/img/Welcome-Fair/spring-lane-building.pdf",
+    map: "https://assets-cdn.sums.digital/YU/website/img/Welcome-Fair/2026/James-Hall.jpg",
   },
   {
     id: "7",
-    map: "https://assets-cdn.sums.digital/YU/website/img/Welcome-Fair/vanbrugh-dining-hall.pdf",
+    map: "https://assets-cdn.sums.digital/YU/website/img/Welcome-Fair/2026/Vanbrugh.jpg",
   },
   {
     id: "8",
-    map: "https://assets-cdn.sums.digital/YU/website/img/Welcome-Fair/roger-kirk-centre.pdf",
+    map: "https://assets-cdn.sums.digital/YU/website/img/Welcome-Fair/2026/Roger-Kirk-Centre.jpg",
   },
 ];
